c1bce353456bd6a1cdc2ed14cc352e3d70b66828
bookworm41 <95772102+aman5213@users.noreply.github.com>
2022-10-27 15:42:01 +0000

    v awsopme

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import random
 
 #MUST be a 4 letter word 
 word_bank = [['v','e','r','y'],['k','i','n','k']]
-words_guessed = ['e','v','k','n']
+words_guessed = []
+answer = ['_','_','_','_']
+wrong_guesses = []
 hangman =  ['''
     ______
     |     |
@@ -62,13 +64,27 @@ hangman =  ['''
     ---------''']
 lives = 0
 word_to_guess = random.choice(word_bank)
-print(hangman[lives])
 place_holder = "_ "
-for i in range(len(words_guessed)):
-  rnd = i
-  for j in range(len(word_to_guess)):
-    if words_guessed[rnd] == word_to_guess[j]:
-      print(words_guessed[rnd])
-print("    ",end = "",)
-for i in range(4):
-  print(place_holder,end = "",)
+while True:
+
+  
+  print(hangman[lives])
+  for i in range(len(words_guessed)):
+    rnd = i
+    for j in range(len(word_to_guess)):
+      stop=False
+      if words_guessed[rnd] == word_to_guess[j]:
+        answer[j]=words_guessed[rnd]
+        stop = True
+      elif words_guessed[rnd] not in wrong_guesses and not stop:
+        lives+=1
+        print('    INCORRECT')
+        wrong_guesses.append(words_guessed[rnd])
+  print("    ",end = "",)
+  for i in range(4):
+    print(answer[i],end = "")
+    print(" ",end = "")
+  print('\n   ',wrong_guesses)
+  Input = input('\n\nGuess a letter:\n>>> ')
+  words_guessed.append(Input)
+  print('\n'*7)
